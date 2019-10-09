6792bd014f626fd7d8516ceef7b3ac1137da14c9
rks764930 <rakeshkumar92215@gmail.com>
2019-10-09 18:21:49 -0400

    day2_swift

---
+++ Day2/main.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+import Darwin
+print("Hello, World!")
+
+var str="aaabbccccdaee"
+print(str)
+var numOfa=0
+for i in qoute
+{
+    if i=="a"
+    {
+        numOfa++
+    }
+    print 
+}
